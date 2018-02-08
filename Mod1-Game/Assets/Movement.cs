55886cad615f0b88110a4689ad5d3aaa4c0e8ba6
Sergio De Anda <35745672+Sergio-De-Anda@users.noreply.github.com>
2018-02-08 15:51:06 -0600

    mod1 game
    1st commit

---
+++ Mod1-Game/Assets/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public Vector3 velocity = new Vector2(0, 0);
+    public float speed = 2f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        transform.position =  new Vector2(0, -3f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+        velocity = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            velocity = new Vector2(-2f, 0);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            velocity = new Vector2(2f, 0);
+        if (Input.GetKey(KeyCode.Space))
+            velocity = new Vector2(velocity.x, 5f);
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+            velocity = new Vector2(0, -5f);
+
+        transform.position = transform.position + (velocity * speed * Time.deltaTime);
+
+        
+    }
+}
