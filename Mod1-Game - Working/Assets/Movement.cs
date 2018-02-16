0b31dad5af6d7fc89c323351d9b8aa7f6fa0221b
braenduhn <35537610+braenduhn@users.noreply.github.com>
2018-02-16 15:46:39 -0600

    Rescale & "Sticky Walls" fixed
    -Fixed issues when colliding with platforms, "Sticky Walls"
    -Rescaled volt and platforms to try different speeds and jump heights

--- Mod1-Game - Working/Assets/Movement.cs
+++ Mod1-Game - Working/Assets/Movement.cs
@@ -4,26 +4,25 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
     public Vector2 jump = new Vector2(0.0f, 10.0f);
+    public float jumpForce = 15f;
+
     public Vector3 velocity = new Vector2(0, 0);
     public float speed = 2f;
+
     Rigidbody2D rb;
     SpriteRenderer mySpriteRenderer;
     bool grounded = true;
     //bool canMove = true;
-    public float jumpForce = 15f;
-   
+
+
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         //transform.position =  new Vector2(0, 0f);
         rb = GetComponent<Rigidbody2D>();
         mySpriteRenderer = GetComponent<SpriteRenderer>();
-        
-        //rb.position = new Vector2(1, 1);
-
-        
+        //rb.position = new Vector2(1, 1); 
     }
     void OnCollisionEnter2D(Collision2D coll)
     {
@@ -35,26 +34,25 @@ public class Movement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate ()
+    void FixedUpdate()
     {
         //Debug.Log(Camera.main.ViewportToWorldPoint(new Vector2(0f, 0f)));
 
         velocity = new Vector2(0, 0);
 
-
-        if (Input.GetKey(KeyCode.LeftArrow))
+        //Move Left
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             mySpriteRenderer.flipX = true;
             velocity = new Vector2(-2f, 0);
-            
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        //Move Right
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             mySpriteRenderer.flipX = false;
             velocity = new Vector2(2f, 0);
-            
         }
-        
+        //Jump
         if (Input.GetKeyDown(KeyCode.Space) && grounded == true)
         {
             rb.AddForce(jump * jumpForce, ForceMode2D.Impulse);
@@ -62,6 +60,6 @@ public class Movement : MonoBehaviour
             //canMove = false;
         }
         transform.position = transform.position + (velocity * speed * Time.deltaTime);
-        
+
     }
 }
