b8cd46f11b511b9cb3ad031d3ce2b548ddbc3d14
braenduhn <35537610+braenduhn@users.noreply.github.com>
2018-02-17 16:11:52 -0600

    Movement & Health System
    -Created a new script for movement and jumping, still tewaking it.
    -Added a Health system (5 HP) that can be modified off of the Player Script to see change for now

---
+++ mod1_Gravy_Goats_Brandon/Assets/Move_Camera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Camera : MonoBehaviour {
+
+    Vector2 cam;
+    // Use this for initialization
+    void Start()
+    {
+        //Debug.Log(Camera.main.ScreenToWorldPoint(new Vector2(Camera.main.pixelWidth/2, Camera.main.pixelHeight/2)));
+        //cam = Camera.main.ScreenToWorldPoint(new Vector2(Camera.main.pixelWidth, Camera.main.pixelHeight));
+        cam = Camera.main.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > cam.x + 10f)
+        {
+
+            Vector2 temp = Camera.main.transform.position;
+            Camera.main.transform.position = new Vector3(temp.x + 20f, temp.y, -10);
+            //cam = Camera.main.ScreenToWorldPoint(new Vector2(Camera.main.pixelWidth, Camera.main.pixelHeight));
+            cam = Camera.main.transform.position;
+        }
+        
+        else if (transform.position.x < cam.x - 10f)
+        {
+            Vector2 temp = Camera.main.ViewportToWorldPoint(new Vector2(0f, 0f));
+            transform.position = new Vector2(temp.x + 1f, transform.position.y);
+        }
+        
+    }
+}
---
+++ mod1_Gravy_Goats_Brandon/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+    private Player player; //access variables in Player Script
+    void Start()
+    {
+        player = gameObject.GetComponentInParent<Player>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)//is grounded
+    {
+        player.grounded = true;
+    }
+
+    void OnTriggerExit2D(Collider2D col)//no longer grounded
+    {
+        player.grounded = false;
+    }
+
+    void OnTriggerStay2D(Collider2D col) //called when trigger stays in something fixes bug where gorunded did not get set to true sometimes
+    {
+        player.grounded = true;
+    }
+}
