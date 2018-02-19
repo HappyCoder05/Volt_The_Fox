280f1edcf84f7f0a36720c549d97c57a944589ea
Brandon <brandon@MacBook-Pro-3.local>
2018-02-19 16:33:57 -0600

    First

---
+++ Assets/Materials/Plummet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//i added these lines
+public class Plummet : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Collider is on trigger.");
+        Vector3 velocity = new Vector2(0, -20f);
+
+        transform.position = transform.position + (velocity * Time.deltaTime);
+
+    }
+}
---
+++ Assets/Scripts/trap.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trap : MonoBehaviour
+{
+    private Rigidbody2D platform_rb;
+    private Vector2 starting_pos;
+	
+
+	void Start ()
+    {
+        platform_rb = GetComponent<Rigidbody2D>();
+        starting_pos = transform.position;
+	}
+	
+	
+	void Update ()
+    {
+		if(transform.position.y < 0 && gameObject.name == "trap")
+        {
+            platform_rb.gravityScale = 0.0f;
+            transform.position = starting_pos;
+        }
+        if(gameObject.name == "moving_platform")
+        {
+            if(transform.position.y < 2.0f)
+            {
+                platform_rb.gravityScale = -.75f;
+            }
+            else if(transform.position.y > 8.0f)
+            {
+                platform_rb.gravityScale = .75f;
+            }
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(gameObject.name == "trap")
+            platform_rb.gravityScale = 0.25f;
+        //rigidbody.gravityScale = 0.1f;
+    }
+}
