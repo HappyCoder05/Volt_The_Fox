a838574c41e14006317507bee037ab7b1e7c77c5
HappyCoder05 <sergio.vela01@utrgv.edu>
2018-02-22 16:03:22 -0600

    Created script for tree traps.

---
+++ Assets/Scripts/Tree_Trap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree_Trap : MonoBehaviour {
+
+    Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    void OnTriggerEnter2D (Collider2D col)
+    {
+        if (col.gameObject.name.Equals("player"))
+            rb.isKinematic = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+            Debug.Log("got him");
+            
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
